b424fe4cdefe614772aecb1a27e70b43d9b9f1ce
Hugo Veiga <141176099+HugoVS26@users.noreply.github.com>
2026-04-16 16:18:01 +0200

    feat: add new router with endpoint to DebtListPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import type { JSX } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import router from '@/router/router';
 
 function App(): JSX.Element {
-  return <main></main>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
